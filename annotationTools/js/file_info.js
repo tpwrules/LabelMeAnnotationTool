87252f3b7c94ababcc67d2a7bfd2e49a8e1f1ee9
Thomas Watson <twatson52@icloud.com>
2020-04-03 15:21:11 -0500

    add home button on toolbar

--- annotationTools/js/file_info.js
+++ annotationTools/js/file_info.js
@@ -59,9 +59,9 @@ function file_info() {
 					else{
 						$('#prevImage').show();
 						if (bname == 'Netscape'){
-							$('#label_buttons_contrast').css('left', '440px');
+							$('#label_buttons_contrast').css('left', '465px');
 						}
-						else $('#label_buttons_contrast').css('left', '420px');
+						else $('#label_buttons_contrast').css('left', '455px');
 
 					}
                     if(this.mode=='im' || this.mode=='mt') view_ObjList = false;
